9fe02631e664596004c188f12632aea5f2a20328
Hammad Ahmed <syedhammad283@gmail.com>
2026-03-28 23:15:54 +0500

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -292,7 +292,7 @@ function About() {
             Our Core Values
           </h2>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 cursor-pointer ">
             {[
               {
                 icon: <FaHeart className="text-3xl md:text-4xl" />,
